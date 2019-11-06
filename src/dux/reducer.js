7db8eff9f8a4993c7ab58049eefba196a74592b2
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-05 19:17:45 -0700

    change

---
+++ src/dux/reducer.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+  
+const initialState = {
+    items: [],
+    loading: false,
+    error: ''
+}
+const GET_ITEMS = "GET_ITEMS"
+
+export default function reducer(state = initialState, action){
+    let {type, payload} = action
+    switch(type){
+        case GET_ITEMS + "_PENDING":
+            return{...state, loading: true}
+        case GET_ITEMS + "_FULFILLED":
+            return {...state, state: payload, loading: false}
+        case GET_ITEMS + "_REJECTED":
+            return {...state, loading: false, error: payload}
+
+
+        default:
+            return state 
+    }
+}
+
+export function getItems(){
+    let items = axios.get('/api/items')
+        .then(res => {
+            return res.data
+        })
+        console.log(items)
+        return {
+            type: GET_ITEMS,
+            payload: items
+        }
+    
+}
